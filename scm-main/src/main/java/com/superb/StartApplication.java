bcfc10c129baea3908119b87ea990c32cc7a2316
gaochao126 <15310559572>
2020-12-16 18:10:04 +0800

    feat: config

--- scm-main/src/main/java/com/superb/StartApplication.java
+++ scm-main/src/main/java/com/superb/StartApplication.java
@@ -3,12 +3,14 @@ package com.superb;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author chao.gao
  */
-@SpringBootApplication
-@MapperScan(basePackages = "com.superb.mapper")
+@SpringBootApplication(scanBasePackages = "com.superb")
+@MapperScan(basePackages = {"com.superb.mapper", "com.superb.config"})
+@ComponentScan(basePackages = {"com.superb", "com.superb.config", "com.superb.config.interceptor", "com.superb.config.filter"})
 public class StartApplication {
 
     public static void main(String[] args) {
