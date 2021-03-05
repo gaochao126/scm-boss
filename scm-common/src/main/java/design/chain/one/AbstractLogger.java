20002f2c4d2467f8d2abbe088f72202d72d2c262
gaochao126 <15310559572>
2021-03-05 15:33:24 +0800

    feat： 责任链模式

---
+++ scm-common/src/main/java/design/chain/one/AbstractLogger.java
@@ -0,0 +1,36 @@
+package design.chain.one;
+
+import java.util.Objects;
+
+/**
+ * 打印日志抽象类
+ * <p>
+ * 不同日志基本类，继承该类，形成责任链模式
+ */
+public abstract class AbstractLogger {
+
+    public static int INFO = 1;
+    public static int DEBUG = 2;
+    public static int ERROR = 3;
+
+    // 日志等级
+    protected int level;
+
+    private AbstractLogger nextLogger;
+
+    public void setNextLogger(AbstractLogger nextLogger) {
+        this.nextLogger = nextLogger;
+    }
+
+    public void logMsg(int level, String msg) {
+        if (this.level <= level) {
+            write(msg);
+        }
+        if (!Objects.isNull(nextLogger)) {
+            nextLogger.logMsg(level, msg);
+        }
+    }
+
+    abstract protected void write(String msg);
+
+}
